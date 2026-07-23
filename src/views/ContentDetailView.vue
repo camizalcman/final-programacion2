0e2882242fe68e0a66fe4df4691d30d66e93cc30
camizalcman <camizalcman@gmail.com>
2026-07-23 00:57:45 -0300

    Comentario explicacion

--- src/views/ContentDetailView.vue
+++ src/views/ContentDetailView.vue
@@ -14,6 +14,7 @@ const destination = computed(() =>
   contentStore.items.find((item) => item.id === Number(route.params.id))
 )
 
+//solo si contentStore.items esta vacía, hace fetch. Si no está vacia es porque ya tiene los datos
 onMounted(() => {
   if (contentStore.items.length === 0) {
     contentStore.fetchContent()
